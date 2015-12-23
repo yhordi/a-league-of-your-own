188963adfaea92291600bf357831dfb5787a524a
yhordi <jordan@devbootcamp.com>
2015-12-22 17:08:04 -0800

    Tests Game

--- test/models/game_test.rb
+++ test/models/game_test.rb
@@ -1,11 +1,21 @@
 require "test_helper"
 
 class GameTest < ActiveSupport::TestCase
-  def game
-    @game ||= Game.new
+  def setup
+    @game = games(:soccer)
+    @user = users(:rodge)
+    @game.users << @user
+  end
+
+  def test_standings
+    assert_kind_of Standing::ActiveRecord_Associations_CollectionProxy, @game.standings
+  end
+
+  def test_users_through_standings
+    assert_includes @game.users, @user
   end
 
   def test_valid
-    assert game.valid?
+    assert @game.valid?
   end
 end
